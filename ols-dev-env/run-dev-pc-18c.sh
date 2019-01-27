feb180fb5d22ffe6f6fc2a281f52c8c2ea69177e
Marcelo Ochoa <mochoa@scotas.com>
2019-01-27 17:14:46 -0300

    use source code from github checkout

--- ols-dev-env/run-dev-pc-18c.sh
+++ ols-dev-env/run-dev-pc-18c.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker run -ti --rm --name dev-pc-18c --hostname dev --link pc:ols \
--v /home/oracle/ols_12c:/home/ols \
--v /home/oracle/lucene_solr_4_0/solr:/home/lucene_solr_4_0/solr \
--v /home/oracle/lucene_solr_4_0/lucene:/home/lucene_solr_4_0/lucene \
+-v /home/oracle/github:/home/ols \
 ols-dev:2.0.4
