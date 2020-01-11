8003e5b8ef5e28db4dfe17c1c6b40b658c732e0f
Marcelo Ochoa <mochoa@scotas.com>
2020-01-11 18:42:47 -0300

    layer image for ols-dev r5

--- ols-dev-env/run-dev-r5.sh
+++ ols-dev-env/run-dev-r5.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker run -ti --rm --name dev-r5 --hostname dev --link 19c:ols \
 -v /home/oracle/github/ols:/home/ols \
--v /home/oracle/lucene_solr_4_0/solr:/home/lucene_solr_4_0/solr \
--v /home/oracle/lucene_solr_4_0/lucene:/home/lucene_solr_4_0/lucene \
 ols-dev:2.0.5
