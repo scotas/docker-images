cce6acdd87c7018b1ef17c881e93f150ee8e6875
Marcelo Ochoa <mochoa@scotas.com>
2020-11-25 12:29:27 -0300

    changed http by https

--- ols-scripts-r4/00-unzip-ols.sh
+++ ols-scripts-r4/00-unzip-ols.sh
@@ -5,12 +5,12 @@ ANT_CONTRIB_VERSION=1.0b3
 curl -s -o /home/oracle/apache-ant-"$ANT_VERSION"-bin.zip http://www-us.apache.org/dist/ant/binaries/apache-ant-"$ANT_VERSION"-bin.zip
 curl -s -L -o /home/oracle/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip https://sourceforge.net/projects/ant-contrib/files/ant-contrib/"$ANT_CONTRIB_VERSION"/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip/download
 
-curl -s -o /home/oracle/valid.ols http://www.scotas.com/download/ols/valid.ols
-curl -s -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip http://www.scotas.com/download/ols/18cR1/lucene-ols-bin-"$OLS_VERSION".zip
-curl -s -o /home/oracle/ODCI.jar http://www.scotas.com/download/ols/18cR1/ODCI.jar
-curl -s -o /home/oracle/converter.jar http://www.scotas.com/download/ols/18cR1/converter.jar
-curl -s -o /home/oracle/translator.jar http://www.scotas.com/download/ols/18cR1/translator.jar
-curl -s -o /home/oracle/initsoxx.sql http://www.scotas.com/download/ols/18cR1/initsoxx.sql
+curl -s -o /home/oracle/valid.ols https://www.scotas.com/download/ols/valid.ols
+curl -s -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip https://www.scotas.com/download/ols/18cR1/lucene-ols-bin-"$OLS_VERSION".zip
+curl -s -o /home/oracle/ODCI.jar https://www.scotas.com/download/ols/18cR1/ODCI.jar
+curl -s -o /home/oracle/converter.jar https://www.scotas.com/download/ols/18cR1/converter.jar
+curl -s -o /home/oracle/translator.jar https://www.scotas.com/download/ols/18cR1/translator.jar
+curl -s -o /home/oracle/initsoxx.sql https://www.scotas.com/download/ols/18cR1/initsoxx.sql
 unzip -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip -d /home/oracle/
 unzip -o /home/oracle/apache-ant-"$ANT_VERSION"-bin.zip -d /home/oracle/
 unzip -o /home/oracle/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip -d /home/oracle/
