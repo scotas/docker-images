882176539f3f487b9711ff093b54ada4a88c4df1
Marcelo Ochoa <mochoa@scotas.com>
2020-11-26 20:56:54 -0300

    change http by https

--- pc-18cXE/00-unzip-pc.sh
+++ pc-18cXE/00-unzip-pc.sh
@@ -4,7 +4,7 @@ if [ ! -f "$FILE" ]; then
     yum -y --enablerepo ol7_optional_latest install ant ant-contrib ant-junit apache-ivy
     yum clean all
     PC_FILE=scotas-pc-bin-2.0.0.zip
-    curl -o $ORACLE_BASE/scripts/setup/$PC_FILE http://www.scotas.com/download/pc/$PC_FILE
+    curl -o $ORACLE_BASE/scripts/setup/$PC_FILE https://www.scotas.com/download/pc/$PC_FILE
     unzip $ORACLE_BASE/scripts/setup/$PC_FILE -d $ORACLE_BASE/scripts/setup
     rm -f $ORACLE_BASE/scripts/setup/$PC_FILE
 fi
