a163048db4dd7670dd4c5b69b20fbac7005b1ec7
Marcelo Ochoa <mochoa@scotas.com>
2020-11-27 14:16:30 -0300

    updated to https urls

--- ols-scripts-r4/00-unzip-ols.sh
+++ ols-scripts-r4/00-unzip-ols.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-OLS_VERSION=2.0.4
+OLS_VERSION=2.0.5
 ANT_VERSION=1.10.9
 ANT_CONTRIB_VERSION=1.0b3
+
 curl -s -L -o /home/oracle/apache-ant-"$ANT_VERSION"-bin.zip https://www-us.apache.org/dist/ant/binaries/apache-ant-"$ANT_VERSION"-bin.zip
 curl -s -L -o /home/oracle/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip https://sourceforge.net/projects/ant-contrib/files/ant-contrib/"$ANT_CONTRIB_VERSION"/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip/download
 
-curl -s -o /home/oracle/valid.ols https://www.scotas.com/download/ols/valid.ols
-curl -s -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip https://www.scotas.com/download/ols/18cR1/lucene-ols-bin-"$OLS_VERSION".zip
-curl -s -o /home/oracle/ODCI.jar https://www.scotas.com/download/ols/18cR1/ODCI.jar
-curl -s -o /home/oracle/converter.jar https://www.scotas.com/download/ols/18cR1/converter.jar
-curl -s -o /home/oracle/translator.jar https://www.scotas.com/download/ols/18cR1/translator.jar
-curl -s -o /home/oracle/initsoxx.sql https://www.scotas.com/download/ols/18cR1/initsoxx.sql
+curl -s -L -o /home/oracle/valid.ols "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/valid.ols"
+curl -s -L -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/lucene-ols-bin-$OLS_VERSION.zip"
+curl -s -L -o /home/oracle/ODCI.jar "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/ODCI.jar"
+curl -s -L -o /home/oracle/converter.jar "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/converter.jar"
+curl -s -L -o /home/oracle/translator.jar "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/translator.jar"
+curl -s -L -o /home/oracle/initsoxx.sql "https://github.com/scotas/ols/releases/download/v$OLS_VERSION/initsoxx.sql"
 unzip -o /home/oracle/lucene-ols-bin-"$OLS_VERSION".zip -d /home/oracle/
 unzip -o /home/oracle/apache-ant-"$ANT_VERSION"-bin.zip -d /home/oracle/
 unzip -o /home/oracle/ant-contrib-"$ANT_CONTRIB_VERSION"-bin.zip -d /home/oracle/
