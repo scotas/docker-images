fce2d4815b7d607eeb3a21ae875dd91b802d2464
Marcelo Ochoa <marcelo.ochoa@gmail.com>
2026-09-09 11:59:36 -0300

    Update OLS scripts and Docker configuration for improved setup and functionality

--- run-ols-r6.sh
+++ run-ols-r6.sh
@@ -4,6 +4,9 @@ docker run -d \
 -e ORACLE_PASSWORD=Oracle_2026 \
 -e ORACLE_SID=FREE \
 -e ORACLE_PDB=FREEPDB1 \
+-e APP_USER=scott \
+-e APP_USER_PASSWORD=tiger \
 -p 2521:1521 -p 8080:8080 -p 9099:9099 \
+-v /run/shm:/dev/shm \
 -v "$PWD/ols-scripts-r6:/container-entrypoint-initdb.d/" \
 gvenzl/oracle-free:23.26.2-full
